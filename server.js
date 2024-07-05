1c1ed82e2032fe9e72c7c4c5d65edf1d31c7e506
Jorge Andrade <>
2024-07-05 09:53:24 -0400

    add cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const configRoof = require('./config.json')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const fs = require('fs');
 
 const app = express ();
 
-
+app.use(cors({
+    origin: '*'
+}));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.json());
